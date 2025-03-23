68c9180a549ea6b4d597caba8244998e1a394b44
Habiba Ansari <147974826+Habiba-Ansari@users.noreply.github.com>
2025-03-23 20:10:32 +0530

    Update priority_scheduling.py
    had some error in the old version just corrected it

--- priority_scheduling.py
+++ priority_scheduling.py
@@ -2,14 +2,17 @@ def ps(pro, bt, pr):
     n=len(pro)
     pro, bt, pri=zip(*sorted(zip(pro, bt, pr), key=lambda x:x[2]))
 
+    wt=[0]*n
+    tat=[0]*n
     for i in range(1, n):
         wt[i]=wt[i-1]+bt[i-1]
 
     for i in range(n):
         tat[i]=wt[i]+bt[i]
 
-    print("Process\tBT\tWT\tTAT")
-    print(f"P{pro[i}\t{bt[i]}\t{wt[i]}\t{tat[i]}")
+    print("Process\tPri\tBT\tWT\tTAT")
+    for i in range(n):
+        print(f"P{pro[i]}\t{pr[i]}\t{bt[i]}\t{wt[i]}\t{tat[i]}")
 
 pro=[1,2,3,4,5]
 bt=[1,7,3,8,2]
